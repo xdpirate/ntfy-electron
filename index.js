a00c7dd244d698a4b40326bf225f8392443b8554
xdpirate <1757462+xdpirate@users.noreply.github.com>
2023-04-28 13:28:56 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, Tray } = require('electron');
+
+let win, tray;
+let winHidden = 0;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+  });
+  
+  win.menuBarVisible = false;
+  win.loadURL("https://ntfy.sh/app");
+  
+  win.on('closed', () => {
+    win = null;
+  });
+  
+  win.webContents.on('new-window', (event, url) => {
+    event.preventDefault();
+    win.loadURL(url);
+  });
+  
+  tray = new Tray("ntfy.png");
+  tray.setToolTip("ntfy-electron | click to hide/show main window");
+  
+  tray.on("click", function() {
+    if(winHidden) {
+      winHidden = 0;
+      win.show();
+    } else {
+      winHidden = 1;
+      win.hide();
+    }
+  });
+}
+
+app.on('ready', createWindow);
