fc5a2a99cea05829677679133a02769d66ae277a
xdpirate <1757462+xdpirate@users.noreply.github.com>
2023-08-03 22:44:44 +0200

    Add support for non-official instances, closes #1

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Dead simple Electron wrapper for the ntfy web app.
 ## Features
 * Tray icon, click it to hide/show the main window
 * Native notifications
+* Support for non-official ntfy instances
 
 ## Command-line arguments
 `--hidden`<br />
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const { app, BrowserWindow, Tray } = require('electron');
+const { app, BrowserWindow, Tray, Menu, MenuItem } = require('electron');
 const process = require('process');
+const path = require('path');
+const Store = require('./store.js');
 
 console.log(process.argv);
 
@@ -7,53 +9,86 @@ let win, tray;
 let winHidden = 0;
 let appIconLoc = app.getAppPath() + "/ntfy.png";
 
+const store = new Store({
+    configName: 'prefs',
+    defaults: {
+      instanceURL: "https://ntfy.sh/app"
+    }
+});
+
+const prompt = require("custom-electron-prompt");
+
+Menu.setApplicationMenu(Menu.buildFromTemplate([
+    {
+       label: 'Options',
+       submenu: [
+          {
+             label: "Set ntfy instance URL",
+             click: function() {
+                 prompt({
+                     title: "ntfy-electron",
+                     label: 'ntfy instance URL:',
+                     value: store.get("instanceURL"),
+                     inputAttrs: {
+                         type: 'url'
+                     },
+                     type: 'input'
+                 })
+                 .then((response) => {
+                     if((response !== null)) {
+                         store.set("instanceURL", response);
+                         win.loadURL(response);
+                     }
+                 })
+                 .catch(console.error);
+             }
+          }
+       ]
+    }
+]));
+
 function ready() {
-  win = new BrowserWindow({
-    width: 1280,
-    height: 720,
-    icon: appIconLoc,
-    title: "ntfy-electron"
-  });
-
-  win.menuBarVisible = false;
-  win.loadURL("https://ntfy.sh/app");
-  
-  win.on('closed', () => {
-    win = null;
-  });
-  
-  win.webContents.on('new-window', (event, url) => {
-    event.preventDefault();
-    win.loadURL(url);
-  });
-  
-  tray = new Tray(appIconLoc);
-  tray.setToolTip("ntfy-electron | click to hide/show main window");
-  
-  tray.on("click", function() {
-    if(winHidden) {
-      winHidden = 0;
-      win.show();
-    } else {
-      winHidden = 1;
-      win.hide();
+    win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        icon: appIconLoc,
+        title: "ntfy-electron"
+    });
+    
+    win.loadURL(store.get("instanceURL"));
+    
+    win.on('closed', () => {
+        win = null;
+    });
+    
+    win.webContents.on('new-window', (event, url) => {
+        event.preventDefault();
+        win.loadURL(url);
+    });
+    
+    tray = new Tray(appIconLoc);
+    tray.setToolTip("ntfy-electron | click to hide/show main window");
+    
+    tray.on("click", function() {
+        if(winHidden) {
+            winHidden = 0;
+            win.show();
+        } else {
+            winHidden = 1;
+            win.hide();
+        }
+    });
+    
+    win.on("page-title-updated", (event) => {
+        event.preventDefault();
+    });
+    
+    for(i = 0; i < process.argv.length; i++) {
+        if(process.argv[i] == "--hidden") {
+            winHidden = 1;
+            win.hide();
+        }
     }
-  });
-
-  win.on("page-title-updated", (event) => {
-    event.preventDefault();
-  });
-
-  console.log("process.argv[0] == " + process.argv[0]);
-  console.log("process.argv[1] == " + process.argv[1]);
-  console.log("process.argv[2] == " + process.argv[2]);
-
-  for(i = 0; i < process.argv.length; i++) {
-    if(process.argv[i] == "--hidden") {
-      winHidden = 1;
-      win.hide();
-    }
-  }
 }
 
 app.on('ready', ready);
